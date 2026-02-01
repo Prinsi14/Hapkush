7802a2dbc83855e531bb9db9ce0091b0432a1c9b
Prinsi Dwivedi <prinsi75dwivedi@gmail.com>
2026-02-01 06:06:14 +0530

    Initial project setup: Vite + React + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      {/* Header */}
+      <header className="bg-white shadow p-4 text-center font-bold text-xl">
+        MemeGram
+      </header>
+
+      {/* Main content */}
+      <main className="p-4">
+        <p className="text-gray-600">Welcome to MemeGram! Your memes, your feed.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
